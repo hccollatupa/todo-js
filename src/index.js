16b70901630c40e6678675316834ea1572cb25e1
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-24 18:36:55 -0500

    Inicialización del proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { saludar } from './js/componentes.js';
+import './styles.css';
+import img from './assets/webpack.png';
+
+const nombre = 'Hector';
+
+saludar(nombre);
---
+++ webpack.prod.js
@@ -0,0 +1,77 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    },
+    output: {
+        filename: 'main.[contenthash].js',
+        clean: true,
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false,
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                        name: 'assets/[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' },
+            ]
+        }),
+        new TerserPlugin(),
+    ]
+}
