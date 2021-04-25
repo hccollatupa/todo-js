9fe3367c0aa16c57a653a95938e87a2d5c916ea9
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-25 00:30:20 -0500

    Guardando y recuperando Todos

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,20 +1,24 @@
 export class TodoList {
     constructor() {
-        this.todos = []
+        //this.todos = [];
+        this.cargarLocalStorage();
     }
 
     nuevoTodo(todo) {
         this.todos.push(todo);
+        this.guardarLocalStorage();
     }
 
     eliminarTodo(id) {
         this.todos = this.todos.filter(todo => todo.id != id);
+        this.guardarLocalStorage();
     }
 
     marcarCompletado(id) {
         for (const todo of this.todos) {
             if (todo.id == id) {
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -22,13 +26,25 @@ export class TodoList {
 
     eliminarCompletados() {
         this.todos = this.todos.filter(todo => !todo.completado);
+        this.guardarLocalStorage();
     }
 
     guardarLocalStorage() {
-
+        localStorage.setItem('todo', JSON.stringify(this.todos));
     }
 
     cargarLocalStorage() {
+        /*
+        if (localStorage.getItem('todo')) {
+            this.todos = JSON.parse(localStorage.getItem('todo'));
+            console.log('cargar localStorage:', this.todos);
+            console.log(typeof this.todos);
+        } else {
+            this.todos = [];
+        }
+        */
 
+        this.todos = (localStorage.getItem('todo')) ?
+            JSON.parse(localStorage.getItem('todo')) : [];
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,22 +4,5 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-//const tarea1 = new Todo('Aprender Javascript');
-//const tarea2 = new Todo('Comprar un unicornio');
-
-//todoList.nuevoTodo(tarea1);
-//tarea1.completado = true;
-//todoList.nuevoTodo(tarea2);
-
-//console.log(todoList);
-
-//crearTodoHtml(tarea1);
-
-//localStorage.setItem('mi-key', 'ABC123');
-//sessionStorage.setItem('mi-key', 'ABC123');
-
-/*
-setTimeout(() => {
-    localStorage.removeItem('mi-key');
-}, 1500);
-*/
+//todoList.todos.forEach(todo => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml);
