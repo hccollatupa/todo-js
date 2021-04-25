3f1e563d6e41ae2ee40f6c4baf4ee528c0255e52
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-24 20:10:59 -0500

    Eliminar todos completados

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,6 +4,7 @@ import { todoList } from '../index';
 //Referencias en el HTML
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
 
 export const crearTodoHtml = (todo) => {
     const htmlTodo = `
@@ -41,5 +42,15 @@ divTodoList.addEventListener('click', (event) => {
     } else if (nombreElemento.includes('button')) { //hay que borrar el todo
         todoList.eliminarTodo(todoId);
         divTodoList.removeChild(todoElemento);
+    }
+});
+
+btnBorrar.addEventListener('click', (event) => {
+    todoList.eliminarCompletados();
+    for (let i = divTodoList.children.length - 1; i >= 0; i--) {
+        const elemento = divTodoList.children[i];
+        if (elemento.classList.contains('completed')) {
+            divTodoList.removeChild(elemento);
+        }
     }
 });
